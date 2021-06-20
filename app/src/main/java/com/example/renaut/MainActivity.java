37f2228f3debfb9aa8bf8eee7751205af72bee51
Ivan <ivan.belov14.01@gmail.com>
2021-06-20 22:18:27 +0700

    Настройка кнопок меню

---
+++ app/src/main/java/com/example/renaut/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.renaut;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.renaut.RoomData.Words;
+import com.example.renaut.RoomData.WordsDatabase;
+import com.example.renaut.RoomData.WordsListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private WordsListAdapter wordsListAdapter;
+    static WordsDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+        loadWordsList();
+
+        FloatingActionButton button = findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddWordsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.RV);
+        recyclerView.setLayoutManager((new LinearLayoutManager(this)));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        db = WordsDatabase.getDbInstance(getApplicationContext());
+        List<Words> wordsList = db.wordsDao().getAllWords();
+        wordsListAdapter = new WordsListAdapter(wordsList);
+        recyclerView.setAdapter(wordsListAdapter);
+    }
+    private void loadWordsList(){
+        WordsDatabase db = WordsDatabase.getDbInstance(this.getApplicationContext());
+        List<Words> wordsList = db.wordsDao().getAllWords();
+        wordsListAdapter.setUserList(wordsList);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 100){
+            loadWordsList();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadWordsList();
+    }
+}
--- app/src/main/java/com/example/renaut/MainMenu.java
+++ app/src/main/java/com/example/renaut/MainMenu.java
@@ -2,13 +2,26 @@ package com.example.renaut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button buttonBack = (Button)findViewById (R.id.B1);
+        buttonBack.setOnClickListener (this);
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
 }
